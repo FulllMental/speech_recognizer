ff09c78fdf63252cf0dfe0f4e819a67647e56dcb
FulllMental <104234625+FulllMental@users.noreply.github.com>
2023-12-26 22:02:25 +0300

    add VK bot

---
+++ run.py
@@ -0,0 +1,70 @@
+import os
+import json
+
+from dotenv import load_dotenv
+from google.cloud import dialogflow
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    """Create an intent of the given intent type."""
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts: # questions
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        # Here we create a new training phrase for each provided part.
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def list_intents(project_id):
+    from google.cloud import dialogflow
+
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+
+    intents = intents_client.list_intents(request={"parent": parent})
+
+    for intent in intents:
+        print("=" * 20)
+        print("Intent name: {}".format(intent.name))
+        print("Intent display_name: {}".format(intent.display_name))
+        print("Action: {}\n".format(intent.action))
+        print("Root followup intent: {}".format(intent.root_followup_intent_name))
+        print("Parent followup intent: {}\n".format(intent.parent_followup_intent_name))
+
+        print("Input contexts:")
+        for input_context_name in intent.input_context_names:
+            print("\tName: {}".format(input_context_name))
+
+        print("Output contexts:")
+        for output_context in intent.output_contexts:
+            print("\tName: {}".format(output_context.name))
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    telegram_token = os.getenv('GOOGLE_API_KEY')
+    project_id = os.getenv('PROJECT_ID')
+    with open('new_phrases.json', 'r', encoding='utf-8') as new_phrases:
+        phrases = new_phrases.read()
+    all_phrases = json.loads(phrases)
+    for phrase_type in all_phrases:
+        training_phrases_parts = all_phrases[phrase_type]['questions']
+        message_texts = [all_phrases[phrase_type]['answer']]
+        create_intent(project_id, phrase_type, training_phrases_parts, message_texts)
