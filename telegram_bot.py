aead8324da8fb17b24022abda86bfa16bc1f23e3
FulllMental <104234625+FulllMental@users.noreply.github.com>
2024-01-15 23:41:11 +0300

    Change telegram_bot reply logic

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,6 +8,8 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 
 from dialogflow_handler import detect_intent_texts
 
+logger = logging.getLogger(__name__)
+
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте!')
@@ -18,17 +20,13 @@ def reply(update: Update, context: CallbackContext) -> None:
     session_id = update.effective_user
     text = update.message.text
     answer = detect_intent_texts(project_id, session_id, text, 'ru')
-    if not answer['is_fallback']:
-        update.message.reply_text(answer['fulfillment_text'])
-    else:
-        update.message.reply_text('К сожалению, я не понимаю вас...')
+    update.message.reply_text(answer['fulfillment_text'])
 
 
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
     )
-    logger = logging.getLogger(__name__)
     load_dotenv()
     telegram_token = os.getenv('TG_BOT_TOKEN')
     tg_user_id = os.getenv('TG_USER_ID')
